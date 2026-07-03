380b5263b64365df1e71fcde9ebc048a79b3b5dd
sakata yumari <bb38124044@ms.nagasaki-u.ac.jp>
2026-07-03 15:24:02 +0900

    commit

---
+++ handson.py
@@ -0,0 +1,21 @@
+import pyagme
+
+
+#画面サイズ定義
+W, H = 320, 270
+
+#タイル数
+TILE_X = 16
+TIRE_Y = 14
+
+def main():
+    ''' メイン関数
+    '''
+    #pygame初期化
+    pygame.init()
+    #画面を作成
+    win = pygame.display.set_mode(())
+
+
+if __name__ == '__main__':
+    main()
